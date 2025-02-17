b60cd9a135341ae90691a54c2d48ed1ae81746b3
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-02-17 00:45:40 +0000

    fix typos

--- spec/order_spec.js
+++ spec/order_spec.js
@@ -6,13 +6,13 @@ describe("Tests all stages of an order", function() {
         const aResults = oOrder.handleInput("hello");
         expect(aResults[0]).toBe("Welcome to Chatime! Let’s get started on an order for you today.")
     });
-    it("test size regular", function() {
+    it("test start order", function() {
         const oOrder = new Order("002-028-2025");
         oOrder.handleInput("hello")
         const aResults = oOrder.handleInput("yes");
-        expect(aResults[0]).toBe("Your rapid test is reserved under the phone number 999-999-9999")
+        expect(aResults[0]).toBe("What size drink would you like to order: regular [+0.00], or large [+0.70]?")
     });
-    it("test size large", function() {
+    it("test defer order", function() {
         const oOrder = new Order("002-028-2025");
         oOrder.handleInput("hello")
         const aResults = oOrder.handleInput("no");
