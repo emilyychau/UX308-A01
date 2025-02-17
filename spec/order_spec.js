4805ee94785609978477b8c0cc41f7a7bf8ba974
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-02-17 01:01:03 +0000

    spec tests drink type

--- spec/order_spec.js
+++ spec/order_spec.js
@@ -16,7 +16,117 @@ describe("Tests all stages of an order", function() {
         const oOrder = new Order("002-028-2025");
         oOrder.handleInput("hello")
         const aResults = oOrder.handleInput("no");
-        expect(aResults[0]).toBe("Thanks for trying our reservation system")
+        expect(aResults[0]).toBe("No worries, see you soon!")
+    });
+    it("test size regular", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("regular");
+        expect(aResults2[0]).toBe("Which drink would you like?")
+    });
+    it("test size large", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("large");
+        expect(aResults1[0]).toBe("Which drink would you like?")
+    });
+    it("test drink type - regular matcha", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("regular");
+        const aResults2 = oOrder.handleInput("matcha milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - large matcha", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("large");
+        const aResults2 = oOrder.handleInput("matcha milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - regular roasted", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("regular");
+        const aResults2 = oOrder.handleInput("roasted milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - large roasted", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("large");
+        const aResults2 = oOrder.handleInput("roasted milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - regular taro", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("regular");
+        const aResults2 = oOrder.handleInput("taro milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - large taro", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("large");
+        const aResults2 = oOrder.handleInput("taro milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - regular earl grey", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("regular");
+        const aResults2 = oOrder.handleInput("earl grey milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - large earl grey", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("large");
+        const aResults2 = oOrder.handleInput("earl grey milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - regular b/s", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("regular");
+        const aResults2 = oOrder.handleInput("brown sugar milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - large b/s", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("large");
+        const aResults2 = oOrder.handleInput("brown sugar milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - regular jasmine", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("regular");
+        const aResults2 = oOrder.handleInput("jasmine jade milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
+    });
+    it("test drink type - large jasmine", function(){
+        const oOrder = new Order("002-028-2025");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults1 = oOrder.handleInput("large");
+        const aResults2 = oOrder.handleInput("jasmine jade milk tea");
+        expect(aResults2[0]).toBe("What type of milk would you like?")
     });
   });
   
